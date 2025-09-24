4a4a3e2ddb94a5378afab34f2702fbb90562f897
Truong Xuan <xuantruongcoder@gmail.com>
2025-09-24 15:07:03 +0700

    config docker

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,36 +1,25 @@
 #!/bin/sh
-
 set -e
 
-role=${CONTAINER_ROLE:-app}
-env=${APP_ENV:-production}
+APP_ENV=${APP_ENV:-production}
 
-cd /var/www/html
+echo "Starting container in $APP_ENV mode..."
 
-echo "Config app..."
+# Permissions
+mkdir -p bootstrap/cache storage
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R ug+rwx storage bootstrap/cache
 
-if [ "$role" = "app" ]; then
-    mkdir -p bootstrap/cache
-    chown -R www-data:www-data storage bootstrap/cache
-    chmod -R ug+rwx storage bootstrap/cache
-
-    if [ "$env" = "production" ]; then
-        echo "Caching configuration..."
-        php artisan config:cache
-        php artisan route:cache
-        php artisan view:cache
-    else
-        composer update
-        echo "Clear Caching..."
-        php artisan cache:clear
-        php artisan config:clear
-        php artisan route:clear
-    fi
-
+if [ "$APP_ENV" = "production" ]; then
+    echo "Caching configuration..."
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
 else
-    echo "Could not match the container role \"$role\""
-    exit 1
+    echo "Dev mode: clearing cache..."
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
 fi
-
 
 exec "$@"
